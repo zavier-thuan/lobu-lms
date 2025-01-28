00e0e85a80ceb4e434dc3428f833ebd9149f2da6
zavier-thuan <zavier.thuan911@gmail.com>
2025-01-28 17:08:15 +0700

    signup finishing...

---
+++ endpoint.js
@@ -0,0 +1,6 @@
+const endpoint = {
+  signupUrl:
+    "https://script.google.com/macros/s/AKfycbzGSFxqYYBLhLpekFKe7EV3qwRMfv5Kgk8h4KSF3h2ZnucQSfPB8CFW0WqfpDM7LNBBAw/exec?action=signup",
+  loginUrl:
+    "https://script.google.com/macros/s/AKfycbzGSFxqYYBLhLpekFKe7EV3qwRMfv5Kgk8h4KSF3h2ZnucQSfPB8CFW0WqfpDM7LNBBAw/exec?action=login",
+};
